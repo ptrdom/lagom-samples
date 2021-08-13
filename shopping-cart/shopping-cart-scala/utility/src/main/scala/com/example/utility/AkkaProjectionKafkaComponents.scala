cfa4de821c5d08f259bbfcf114105315289208bd
Domantas Petrauskas <dom.petrauskas@gmail.com>
2021-08-13 16:46:39 +0300

    WIP

--- shopping-cart/shopping-cart-scala/utility/src/main/scala/com/example/utility/AkkaProjectionKafkaComponents.scala
+++ shopping-cart/shopping-cart-scala/utility/src/main/scala/com/example/utility/AkkaProjectionKafkaComponents.scala
@@ -12,6 +12,14 @@ trait AkkaProjectionKafkaComponents
   def lagomServer: LagomServer
   def serviceLocator: ServiceLocator
 
+  override def topicPublisherName: Option[String] = super.topicPublisherName match {
+    case Some(other) =>
+      sys.error(
+        s"Cannot provide the kafka topic factory as the default topic publisher since a default topic publisher has already been mixed into this cake: $other"
+      )
+    case None => Some("kafka")
+  }
+
   new AkkaProjectionRegisterTopicProducers(
     lagomServer,
     serviceInfo,
